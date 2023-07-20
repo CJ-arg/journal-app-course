f155c1369f28c24f1290b2cb3f85b7a662ac4198
unknown <janoncarlos@gmail.com>
2023-07-20 10:53:54 -0300

    Routes commit

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 
 import { Route, Routes } from 'react-router'
+import { AuthRoutes } from '../auth/routes/AuthRoutes'
+import { JournalRoutes } from '../journal/routes/JournalRoutes'
 
 export const AppRouter = () => {
   return (
     <Routes>
-
-      <Route />
+      <Route path='auth/*' element={<AuthRoutes />} />
+      <Route path='/*' element={<JournalRoutes />} />
       <Route />
     </Routes>
   )
