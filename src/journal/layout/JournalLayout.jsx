ede48ceeafff839ea87afd6494800f466dc744c4
unknown <janoncarlos@gmail.com>
2023-07-24 10:56:52 -0300

    navbar and sidebar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/material'
-import React from 'react'
-import { NavBar } from '../components';
+import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 240;
 
@@ -8,7 +7,7 @@ export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
       <NavBar drawerWidth={drawerWidth} />
-      {/* sideBar  drawerWidth*/}
+      <SideBar drawerWidth={drawerWidth} />
 
       <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
         {/* toolbar  */}
