7c21771e2218ef651b84fc06333b1bccc31884af
unknown <janoncarlos@gmail.com>
2023-07-24 12:36:50 -0300

    note view

---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,40 @@
+import { SaveOutlined } from '@mui/icons-material'
+import { Button, Grid, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+export const NoteView = () => {
+  return (
+
+    <Grid container direction='row' justifyContent='space-between' sx={{ mb: 1 }}>
+
+      <Grid>
+        <Typography fontSize={39} fontWeight='light'> 28 de diciembre, 2023</Typography>
+
+      </Grid>
+      <Grid item>
+        <Button color='primary' sx={{ padding: 2 }}>
+          <SaveOutlined sx={{ fontSize: 30, mr: 1 }} /> Save
+        </Button>
+        <Grid container>
+          <TextField
+            type="text"
+            variant="filled"
+            fullWidth
+            placeholder="Enter a Title"
+            label="Title"
+            sx={{ border: 'none', mb: 1 }}
+          />
+          <TextField
+            type="text"
+            variant="filled"
+            fullWidth
+            multiline
+            placeholder="What happend today?"
+            minRows={5}
+          />
+        </Grid>
+
+      </Grid>
+    </Grid>
+  )
+}
