a4fe3df4e49c0848842545f1003fb4c9290e0309
unknown <janoncarlos@gmail.com>
2023-07-24 12:46:09 -0300

    end section

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
-import { FireTruck, Mail } from '@mui/icons-material'
-import { IconButton, Typography } from '@mui/material'
+import { AddOutlined, FireTruck, Mail } from '@mui/icons-material'
+import { Button, IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
 import { ImageGallery, NoteView, NothingSelectedView } from '../views'
@@ -9,8 +9,21 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, deleniti repellat, deserunt repellendus quisquam sint nulla magni veniam autem ex expedita quae non quis. Sunt doloremque provident pariatur tempora rerum!</Typography> */}
       {/* <NothingSelectedView /> */}
-      <ImageGallery />
-      {/* <NoteView /> */}
+      {/* <ImageGallery /> */}
+      <NoteView />
+      <IconButton size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}>
+
+        <AddOutlined sx={{ fontSize: 30 }} />
+
+      </IconButton>
     </JournalLayout>
 
   )
