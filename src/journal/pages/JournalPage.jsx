8bee2716ebac81a262a679cad8fef75c7ff89b74
unknown <janoncarlos@gmail.com>
2023-07-24 12:10:09 -0300

    navbar and sidebar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,11 +2,13 @@ import { FireTruck, Mail } from '@mui/icons-material'
 import { IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, deleniti repellat, deserunt repellendus quisquam sint nulla magni veniam autem ex expedita quae non quis. Sunt doloremque provident pariatur tempora rerum!</Typography>
+      {/* <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, deleniti repellat, deserunt repellendus quisquam sint nulla magni veniam autem ex expedita quae non quis. Sunt doloremque provident pariatur tempora rerum!</Typography> */}
+      <NothingSelectedView />
     </JournalLayout>
 
   )
