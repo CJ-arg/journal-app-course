5e36cdf907658aee4ed3fdffc5fd0afa4ff37b81
unknown <janoncarlos@gmail.com>
2023-07-24 12:37:15 -0300

    note view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,13 +2,15 @@ import { FireTruck, Mail } from '@mui/icons-material'
 import { IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views'
+import { ImageGallery, NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, deleniti repellat, deserunt repellendus quisquam sint nulla magni veniam autem ex expedita quae non quis. Sunt doloremque provident pariatur tempora rerum!</Typography> */}
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <ImageGallery />
+      {/* <NoteView /> */}
     </JournalLayout>
 
   )
