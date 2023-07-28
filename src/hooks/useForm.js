b88660decf7a86e8f35772488543dc1e4ea7ed4e
unknown <janoncarlos@gmail.com>
2023-07-28 17:11:46 -0300

    use form store

---
+++ src/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const useForm = (rootForm = {}) => {
+  const [formState, setFormState] = useState(rootForm);
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const onResetForm = () => {
+    setFormState(rootForm);
+  };
+
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+  };
+};
