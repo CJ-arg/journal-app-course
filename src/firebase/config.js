7cebc11edce60d082c9fe44b78c8a7fbbbf403a4
unknown <janoncarlos@gmail.com>
2023-07-31 09:57:57 -0300

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMebViJrqfYXbmBR0xHGOcoohavUbg8cM",
+  authDomain: "react-udemy-0723.firebaseapp.com",
+  projectId: "react-udemy-0723",
+  storageBucket: "react-udemy-0723.appspot.com",
+  messagingSenderId: "526172347417",
+  appId: "1:526172347417:web:5a0a2548eb558c0fbade88",
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
