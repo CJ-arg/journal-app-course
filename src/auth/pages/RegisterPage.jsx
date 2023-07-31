3d1e1e6548700030a91c151b0b8bff3c5dd0ae61
unknown <janoncarlos@gmail.com>
2023-07-31 16:27:48 -0300

    validation

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,29 +1,46 @@
 import { Link as RouterLink } from "react-router-dom";
-import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import React from "react"
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks/useForm";
 
+const formData = {
 
+  email: 'pepguardiola@google.com',
+  password: '123456',
+  displayName: 'Carlos Janon'
+}
+const formValidations = {
+
+  email: [(value) => value.includes('@'), 'the email must have an @'],
+  password: [(value) => value.length >= 6, 'the password must have more than 6 ch'],
+  displayName: [(value) => value.length >= 1, 'Name ir required']
+}
 export const RegisterPage = () => {
+  const { formState, displayName, email, password, isFornValid, displayNameValid, emailValid, passwordValid, onInputChange, } = useForm(formData)
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log(formState);
+  }
   return (
     <AuthLayout title="Create Acount">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label='Name' type="text" placeholder="Nombre completo" fullWidth></TextField>
+            <TextField label='Name' type="text" placeholder="Nombre completo" name="displayName" value={displayName} onChange={onInputChange} error={!displayNameValid} helperText={displayNameValid} fullWidth></TextField>
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label='correo' type="email" placeholder="correo@" fullWidth></TextField>
+            <TextField label='correo' type="email" placeholder="correo@" name="email" value={email} onChange={onInputChange} error={false} fullWidth></TextField>
           </Grid>
         </Grid>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label='password' type="password" placeholder="password*" fullWidth></TextField>
+            <TextField label='password' type="password" placeholder="password*" name="password" value={password} onChange={onInputChange} error={false} fullWidth></TextField>
           </Grid>
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }} >
             <Grid item xs={12} sm={6} sx={{ mt: 1 }}>
-              <Button variant="contained" fullWidth> LOGIN</Button>
+              <Button type="submit" variant="contained" fullWidth> Create Account</Button>
             </Grid>
 
             <Grid container direction='row' justifyContent='end' >
