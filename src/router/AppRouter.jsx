1a6bcc68a14c30bd92b401932578eb992f122de5
unknown <janoncarlos@gmail.com>
2023-08-02 09:26:34 -0300

    routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,3 @@
-
 import { Navigate, Route, Routes } from 'react-router'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
@@ -6,21 +5,12 @@ import { JournalRoutes } from '../journal/routes/JournalRoutes'
 export const AppRouter = () => {
   return (
     <Routes>
-
       {
         (status === 'authenticated')
           ? <Route path="/*" element={<JournalRoutes />} />
           : <Route path="/auth/*" element={<AuthRoutes />} />
       }
-
       <Route path='/*' element={<Navigate to='/auth/login' />} />
-
-      {/* Login y Registro */}
-      {/* <Route path="/auth/*" element={ <AuthRoutes /> } /> */}
-
-      {/* JournalApp */}
-      {/* <Route path="/*" element={ <JournalRoutes /> } /> */}
-
     </Routes>
   )
 }
