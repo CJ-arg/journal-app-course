8e926f0d481aa1e2513e455d0654dce8dfce8b0c
unknown <janoncarlos@gmail.com>
2023-08-03 12:37:54 -0300

    user w password mail

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,36 +1,40 @@
 import {
-  GoogleAuthProvider,
+  signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   signInWithPopup,
+  updateProfile,
 } from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
 
-export const signInWhithGoogle = async () => {
+export const singInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(FirebaseAuth, googleProvider);
-    // const credentials = GoogleAuthProvider.credentialFromResult(result);
-    const { displayName, photoURL, email, uid } = result.user;
+    // const credentials = GoogleAuthProvider.credentialFromResult( result );
+    const { displayName, email, photoURL, uid } = result.user;
+
     return {
       ok: true,
+      // User info
       displayName,
-      photoURL,
       email,
+      photoURL,
       uid,
     };
   } catch (error) {
-    // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
+
     return {
       ok: false,
-      errorCode,
       errorMessage,
     };
   }
 };
-export const registerUserEmailPassword = async ({
+
+export const registerUserWithEmailPassword = async ({
   email,
   password,
   displayName,
@@ -42,9 +46,42 @@ export const registerUserEmailPassword = async ({
       password
     );
     const { uid, photoURL } = resp.user;
-    console.log(resp);
+
+    await updateProfile(FirebaseAuth.currentUser, { displayName });
+
+    return {
+      ok: true,
+      uid,
+      photoURL,
+      email,
+      displayName,
+    };
   } catch (error) {
     console.log(error);
     return { ok: false, errorMessage: error.message };
   }
+};
+
+export const loginWithEmailPassword = async ({ email, password }) => {
+  try {
+    const resp = await signInWithEmailAndPassword(
+      FirebaseAuth,
+      email,
+      password
+    );
+    const { uid, photoURL, displayName } = resp.user;
+
+    return {
+      ok: true,
+      uid,
+      photoURL,
+      displayName,
+    };
+  } catch (error) {
+    return { ok: false, errorMessage: error.message };
+  }
+};
+
+export const logoutFirebase = async () => {
+  return await FirebaseAuth.signOut();
 };
