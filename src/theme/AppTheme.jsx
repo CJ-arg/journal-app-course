072ba9f77f90e72c283fea7e52794fc37c92925f
unknown <janoncarlos@gmail.com>
2023-08-07 17:50:49 -0300

    main

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import purpleTheme from './purpleTheme';
 
+
 export const AppTheme = ({ children }) => {
   return (
     <ThemeProvider theme={purpleTheme}>
