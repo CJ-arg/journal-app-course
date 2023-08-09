3d2d2e78596f478b59b25ac89b057bc975048102
unknown <janoncarlos@gmail.com>
2023-08-09 11:30:37 -0300

    use form

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,16 +1,21 @@
 import { useEffect, useMemo, useState } from "react";
 
-export const useForm = (rootForm = {}, formValidations = {}) => {
-  const [formState, setFormState] = useState(rootForm);
+export const useForm = (initialForm = {}, formValidations = {}) => {
+  const [formState, setFormState] = useState(initialForm);
   const [formValidation, setFormValidation] = useState({});
 
   useEffect(() => {
     createValidators();
   }, [formState]);
-
   useEffect(() => {
-    setFormState(rootForm);
-  }, [rootForm]);
+    setFormState(initialForm);
+  }, [initialForm]);
+  const isFormValid = useMemo(() => {
+    for (const formValue of Object.keys(formValidation)) {
+      if (formValidation[formValue] !== null) return false;
+    }
+    return true;
+  }, [formValidation]);
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -19,27 +24,20 @@ export const useForm = (rootForm = {}, formValidations = {}) => {
       [name]: value,
     });
   };
-  const isFormValid = useMemo(() => {
-    for (const formValue of Object.keys(formValidation)) {
-      if (formValidation[formValue] !== null) return false;
-    }
-    return true;
-  }, [formValidation]);
 
   const onResetForm = () => {
-    setFormState(rootForm);
+    setFormState(initialForm);
   };
-
   const createValidators = () => {
-    const formCheckValues = {};
+    const formCheckedValues = {};
     for (const formField of Object.keys(formValidations)) {
-      const [fn, errorMessage = "Validation Error"] =
-        formValidations[formField];
-      formCheckValues[`${formField}Valid`] = fn(formState[formField])
+      const [fn, errorMessage] = formValidations[formField];
+      formCheckedValues[`${formField}Valid`] = fn(formState[formField])
         ? null
         : errorMessage;
     }
-    setFormValidation(formCheckValues);
+
+    setFormValidation(formCheckedValues);
   };
 
   return {
--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,14 +1,28 @@
 import { SaveOutlined } from '@mui/icons-material'
 import { Button, Grid, TextField, Typography } from '@mui/material'
+import { useForm } from '../../hooks/useForm'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useMemo } from 'react'
+import { setActiveNote } from '../../store/journal/journalSlice'
 
 export const NoteView = () => {
+  const dispatch = useDispatch();
+  const { active: note } = useSelector(state => state.journal)
+  const { body, title, onInputChange, formState, date } = useForm(note)
+  const dateString = useMemo(() => {
+    const newDate = new Date(date)
+    return newDate.toUTCString()
+  }, [date])
+  useEffect(() => {
+    dispatch(setActiveNote(formState));
+  }, [formState])
   return (
     <Grid
       className='animate__animated animate__fadeIn animate__faster'
 
       container direction='row' justifyContent='space-between' sx={{ mb: 1 }}>
       <Grid>
-        <Typography fontSize={39} fontWeight='light'> 28 de diciembre, 2023</Typography>
+        <Typography fontSize={39} fontWeight='light'> {dateString}</Typography>
       </Grid>
       <Grid item>
         <Button color='primary' sx={{ padding: 2 }}>
@@ -22,6 +36,9 @@ export const NoteView = () => {
             placeholder="Enter a Title"
             label="Title"
             sx={{ border: 'none', mb: 1 }}
+            name='title'
+            value={title}
+            onChange={onInputChange}
           />
           <TextField
             type="text"
@@ -30,6 +47,9 @@ export const NoteView = () => {
             multiline
             placeholder="What happend today?"
             minRows={5}
+            name='body'
+            value={body}
+            onChange={onInputChange}
           />
         </Grid>
       </Grid>
